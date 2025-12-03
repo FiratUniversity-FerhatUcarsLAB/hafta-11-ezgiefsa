59d80f2d4b5a3a3036934ae437ec51447616bbca
ezgiefsa <250541023@firat.edu.tr>
2025-12-03 13:00:19 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,32 @@
 public class Exercise4_1 {
 
     public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+       System.out.println("===Amerikan Formatı===");
+       System.out.println(day + ", " + mounth + " " + date + ", " + year);
     }
 
     public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+       System.out.println("===Avrupa Formatı===");
+       System.out.println(date + " " + mounth + " " + year + ", " + day);
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+      Scanner input = new Scanner(System.in);
+
+        System.out.print("Gün: ");
+        String day = input.nextLine();
+
+        System.out.print("Ay: ");
+        String month = input.nextLine();
+
+        System.out.print("Tarih : ");
+        int date = input.nextInt();
+
+        System.out.print("Yıl: ");
+        int year = input.nextInt();
+
+        printAmerican(day, month, date, year);
+        printEuropean(date, month, year, day);
+
     }
 }

51941fa648ff4dd82c5fa94968ef3dac72300727
ezgiefsa <250541023@firat.edu.tr>
2025-12-03 13:52:37 +0300

    Enhance Exercise4_3.java with comments and outputs
    Added detailed comments and output examples to Exercise4_3.java.

--- Exercise4_3.java
+++ Exercise4_3.java
@@ -22,6 +22,50 @@ public class Exercise4_3 {
         System.out.println(".");
     }
 }
-ÇIKTISI
-    
+
+main() başlar
+
+print("No, I ");
+Çıktı: No, I
+
+zoop() çağrısı:
+zoop()
+1. baffle()
+print(" wug");
+ping();
+
+
+ping():
+
+println(".");
+
+ wug.
+
+zoop() devam
+print(" You wugga ");
+
+ Çıktı:
+
+ You wugga 
+
+2. baffle() tekrar
+
+Yine:
+
+ wug.
+
+No, I  wug. You wugga  wug.
+
+main() devam:
+print("I ");
+
+Çıktı:
+
+I 
+
+main() → baffle()
+
+Yine:
+ wug.
+
 No, I  wug. You wugga  wug.I  wug.

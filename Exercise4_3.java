360621297a998cdcaece28f73021f76209d5696b
ezgiefsa <250541023@firat.edu.tr>
2025-12-03 21:24:19 +0300

    Clean up Exercise4_3.java by removing comments
    Removed commented-out code and Turkish comments from Exercise4_3.java.

--- Exercise4_3.java
+++ Exercise4_3.java
@@ -23,49 +23,22 @@ public class Exercise4_3 {
     }
 }
 
-main() başlar
-
-print("No, I ");
-Çıktı: No, I
-
+/* main() başlar
+print("No, I "):
 zoop() çağrısı:
-zoop()
 1. baffle()
 print(" wug");
-ping();
-
-
 ping():
-
 println(".");
-
- wug.
-
+wug.
 zoop() devam
 print(" You wugga ");
-
- Çıktı:
-
- You wugga 
-
 2. baffle() tekrar
-
-Yine:
-
- wug.
-
+Yine wug.
 No, I  wug. You wugga  wug.
-
 main() devam:
 print("I ");
-
-Çıktı:
-
-I 
-
 main() → baffle()
-
-Yine:
- wug.
-
-No, I  wug. You wugga  wug.I  wug.
+Yine wug.
+çıktısı 
+No, I  wug. You wugga  wug.I  wug.*/
